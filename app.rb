0521c5cf9bdc14ab2ba701fd96dbf3090cb3acc9
Ellen Gummesson <ellengummesson@gmail.com>
2013-01-23 11:04:00 +0100

    Changed comments.

--- app.rb
+++ app.rb
@@ -1,17 +1,19 @@
-# Gem requirements
+# Gems
 require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
+
+# Standard Library
 require 'date'
 
-# App information
+# Information
 APP_NAME = "Journal."
 APP_DESC = "What did you do today?"
 
 # Set app directory as root
 $:.unshift File.dirname(__FILE__)
 
-# App requirements
+# App Library
 require 'lib/models'
 require 'lib/helpers'
 require 'lib/routes'
