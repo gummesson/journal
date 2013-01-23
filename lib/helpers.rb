a7c100da29c18dce0db7b1fbbfa0913911a7e139
Ellen Gummesson <ellengummesson@gmail.com>
2013-01-23 11:00:43 +0100

    Added documentation.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,21 +1,34 @@
 helpers do
-  # Display message
+  # Displays a message
+  #
+  # message = The given message. Redirects to another page with a different
+  #           layout to display the message.
+  #
   def message(message)
     @message = message
     erb :message
   end
 
-  # Parse date
+  # Parses the date
+  #
+  #  today = yyyy-mm-dd
+  #
   def date
     Date.today
   end
 
-  # Get content
+  # Gets the content
+  #
+  #   params[:content] = The given content from the input field.
+  #
   def content
     params[:content]
   end
 
-  # Get entry id
+  # Gets the entry ID
+  #
+  #  Entry.get params[:id] = Gets the entry ID from the database.
+  #
   def entry_id
     Entry.get params[:id]
   end
